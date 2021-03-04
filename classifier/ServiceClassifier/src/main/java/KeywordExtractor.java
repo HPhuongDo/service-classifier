701bd47f69d9666e0f78ad619fbd41234f13ad1d
Phuong Ha Do <dohaphuong1997@gmail.com>
2021-03-04 23:52:26 +0100

    use StanfordNLP to lemmatize, extract nouns and verbs

---
+++ classifier/ServiceClassifier/src/main/java/KeywordExtractor.java
@@ -0,0 +1,78 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Properties;
+import java.util.Scanner;
+
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.util.CoreMap;
+
+/**
+ * Use StanfordNLP
+ * given a log document, tokenize, lemmatize, extract keywords (verbs and nouns)
+ * @author phuong
+ *
+ */
+public class KeywordExtractor {
+	File file;
+	
+	public KeywordExtractor(File file) {
+		this.file = file;
+	}
+
+	/**
+	 * read file, convert to lower case, remove non-letter characters, extract keywords
+	 * @param file
+	 * @return unique keywords of file
+	 */
+	public HashSet<String> extract() {
+		HashSet<String> words = new HashSet<String>();
+		try {
+	        Scanner sc = new Scanner(this.file);
+	        while (sc.hasNextLine()) {
+	            String line = sc.nextLine();
+	            line = line.replaceAll("[^a-zA-Z ]", " ").toLowerCase();
+	            words.addAll(extractKeywords(line));
+	        }
+	        sc.close();
+	    } 
+	    catch (FileNotFoundException e) {
+	        e.printStackTrace();
+	    }
+		return words;
+	}
+
+	/**
+	 * tokenize into words using character classes
+	 * get POS and Lemma
+	 * @return set of unique words in base form
+	 */
+	private HashSet<String> extractKeywords(String line) {
+		HashSet<String> keywords = new HashSet<String>();
+		Properties props = new Properties();
+        props.setProperty("annotators", "tokenize, ssplit, pos, lemma");
+		StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+		// extract verbs and nouns
+		Annotation document = new Annotation(line);
+		pipeline.annotate(document);
+		List<CoreMap> sentences = document.get(SentencesAnnotation.class);
+		for(CoreMap sentence: sentences) {
+			  // traversing the words in the current sentence
+			  // a CoreLabel is a CoreMap with additional token-specific methods
+			  for (CoreLabel token: sentence.get(TokensAnnotation.class)) {
+				  String pos = token.get(PartOfSpeechAnnotation.class);
+			    if (pos.startsWith("N") || pos.startsWith("V")) {
+					keywords.add(token.lemma());
+				}
+			  }
+		}
+		return keywords;
+	}
+	
+}
--- classifier/ServiceClassifier/src/main/java/Main.java
+++ classifier/ServiceClassifier/src/main/java/Main.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.Properties;
 import java.util.Scanner;
 
+import edu.stanford.nlp.ling.CoreAnnotations.LemmaAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
@@ -35,78 +36,24 @@ import opennlp.tools.tokenize.SimpleTokenizer;
 public class Main {
 	public static final String path = "/home/phuong/Documents/Master-thesis/implementation/services/databases/logs/";
 	
-	public enum Model{
-		OPEN_NLP,
-		UIMA,
-		STANFORD_NLP
-	}
-	
 	public static void main(String[] args) {
 		System.out.println("Starting service classifier...");
 		File mongo = new File(path+"mongo/mongo_queries.log");
 		File mysql = new File(path+"mysql/mysql_queries.log");
-		// extract unique words
-		HashSet<String> mongoWords = readFileToWords(Model.STANFORD_NLP, mongo);
-		HashSet<String> mysqlWords = readFileToWords(Model.STANFORD_NLP, mysql);
 		
-		writeWordsToFile(mongoWords, "src/main/resources/analysis/StanfordNLP/mongo_words.txt");
-		writeWordsToFile(mysqlWords, "src/main/resources/analysis/StanfordNLP/mysql_words.txt");
-		
+		KeywordExtractor mongoExtractor = new KeywordExtractor(mongo);
+		KeywordExtractor mysqlExtractor = new KeywordExtractor(mysql);
+		HashSet<String> mongoWords = mongoExtractor.extract();
+		HashSet<String> mysqlWords = mysqlExtractor.extract();
+
+		writeWordsToFile(mongoWords, "src/main/resources/analysis/lemmatized/mongo_words_queries.txt");
+		writeWordsToFile(mysqlWords, "src/main/resources/analysis/lemmatized/mysql_words_queries.txt");
+
 		// find common words
 		mysqlWords.retainAll(mongoWords);
-		writeWordsToFile(mysqlWords, "src/main/resources/analysis/StanfordNLP/common_words.txt");
+		writeWordsToFile(mysqlWords, "src/main/resources/analysis/lemmatized/common_keywords_queries.txt");
 
-		// extract nouns as keywords
-		HashSet<String> commonKeywords = extractKeywords(Model.STANFORD_NLP, mysqlWords.toArray(new String[mysqlWords.size()]));
-		writeWordsToFile(commonKeywords, "src/main/resources/analysis/StanfordNLP/common_keywords_StanfordNLP.txt");
-		
 		System.out.println("Finished.");
-	}
-	
-	/**
-	 * read file, convert to lower case, remove non-letter characters 
-	 * @param file
-	 * @return
-	 */
-	public static HashSet<String> readFileToWords(Model model, File file) {
-		HashSet<String> words = new HashSet<String>();
-		try {
-	        Scanner sc = new Scanner(file);
-	        while (sc.hasNextLine()) {
-	            String line = sc.nextLine();
-	            line = line.replaceAll("[^a-zA-Z ]", " ").toLowerCase();
-	            words.addAll(tokenize(model, line));
-	        }
-	        sc.close();
-	    } 
-	    catch (FileNotFoundException e) {
-	        e.printStackTrace();
-	    }
-		return words;
-	}
-	
-	/**
-	 * tokenize into words using character classes
-	 */
-	public static List<String> tokenize(Model model, String line) {
-		switch(model) {
-		case OPEN_NLP:
-			SimpleTokenizer simpleTokenizer = SimpleTokenizer.INSTANCE;
-			return Arrays.asList(simpleTokenizer.tokenize(line));
-		case STANFORD_NLP:
-			Reader r = new StringReader(line);
-			PTBTokenizer<Word> ptbTokenizer = PTBTokenizer.newPTBTokenizer(r);
-			ArrayList<String> words = new ArrayList<String>();
-			while (ptbTokenizer.hasNext()) {
-				Word w = ptbTokenizer.next();
-				words.add(w.word());
-			}
-			return words;
-		case UIMA:
-			return null;
-		default:
-			return null;
-		}
 	}
 	
 	/**
@@ -125,57 +72,6 @@ public class Main {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	}
-	
-	/**
-	 * extract only nouns (NN, NNS, NNP, NNPS tags in the Penn Treebank Project)
-	 * @param words
-	 * @return
-	 */
-	public static HashSet<String> extractKeywords(Model model, String[] words) {
-		HashSet<String> keywords = new HashSet<String>();
-		switch (model) {
-		case OPEN_NLP:
-			try {
-				InputStream inputStreamPOSTagger = Main.class.getResourceAsStream("/models/en-pos-perceptron.bin");
-				POSModel posModel = new POSModel(inputStreamPOSTagger);
-				POSTaggerME posTagger = new POSTaggerME(posModel);
-				String tags[] = posTagger.tag(words);
-				// extract nouns
-				for(int i=0; i<words.length; i++) {
-					if (tags[i].startsWith("N")) {
-						keywords.add(words[i]);
-					}
-				}
-				return keywords;
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		case STANFORD_NLP:
-			Properties props = new Properties();
-	        props.setProperty("annotators", "tokenize, ssplit, pos");
-			StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-			// extract nouns
-			String str = String.join(" ", words);
-			Annotation document = new Annotation(str);
-			pipeline.annotate(document);
-			List<CoreMap> sentences = document.get(SentencesAnnotation.class);
-			for(CoreMap sentence: sentences) {
-				  // traversing the words in the current sentence
-				  // a CoreLabel is a CoreMap with additional token-specific methods
-				  for (CoreLabel token: sentence.get(TokensAnnotation.class)) {
-				    // this is the POS tag of the token
-				    String pos = token.get(PartOfSpeechAnnotation.class);
-				    if (pos.startsWith("N")) {
-						keywords.add(token.originalText());
-					}
-				  }
-			}
-			return keywords;
-		default:
-			return null;
-		}
-		
 	}
 	
 }
